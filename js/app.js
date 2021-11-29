1d737f038eef026f7cf6c8df8ef7a6560e5f96fc
viro <twikista@gmail.com>
2021-11-29 20:30:44 +0100

    cache HTML elements and define the gameboard array

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
-const ticTacToeGame = (function () {})();
+const ticTacToeGame = (function () {
+  //cache HTML elements
+  const boardCells = document.querySelector(".game-board");
+  const endOfGameModal = document.querySelector(".end-of-game-modal");
+  const appBody = document.querySelector(".app-body");
+
+  const gameBoard = {
+    //define game board array
+    board: [
+      ["", "", ""],
+      ["", "", ""],
+      ["", "", ""],
+    ],
+  };
+})();
