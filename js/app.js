2a3b4429735d4f7c54fa441ce4ae5b4ee7812a5b
viro <twikista@gmail.com>
2021-11-30 14:09:14 +0100

    added functionality to check board state and reset board

--- js/app.js
+++ js/app.js
@@ -23,5 +23,52 @@ const ticTacToeGame = (function () {
       }
       return count === 9;
     },
+    boardState() {
+      let boardState = null;
+      if (this.boardIsFilled()) {
+        boardState = "draw";
+      }
+      for (i = 0; i < 3; i++) {
+        if (
+          this.board[i][0] !== "" &&
+          this.board[i][0] === this.board[i][1] &&
+          this.board[i][0] === this.board[i][2]
+        ) {
+          boardState = "win";
+        }
+      }
+
+      for (j = 0; j < 3; j++) {
+        if (
+          this.board[0][j] !== "" &&
+          this.board[0][j] === this.board[1][j] &&
+          this.board[0][j] === this.board[2][j]
+        ) {
+          boardState = "win";
+        }
+      }
+
+      for (i = 0; i < 3; i++) {
+        if (
+          (this.board[0][0] !== "" &&
+            this.board[0][0] === this.board[1][1] &&
+            this.board[0][0] === this.board[2][2]) ||
+          (this.board[0][2] !== "" &&
+            this.board[0][2] === this.board[1][1] &&
+            this.board[0][2] === this.board[2][0])
+        ) {
+          boardState = "win";
+        }
+      }
+
+      return boardState;
+    },
+    resetBoard() {
+      for (let i = 0; i < gameBoard.board.length; i++) {
+        for (let j = 0; j < gameBoard.board.length; j++) {
+          gameBoard.board[i][j] = "";
+        }
+      }
+    },
   };
 })();
